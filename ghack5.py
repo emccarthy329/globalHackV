d3dd1848b204ff1ec6e0656c2b258e523d129f54
rcjones <rcjones.iphone@gmail.com>
2015-09-12 12:26:06 -0500

    new db

--- ghack5.py
+++ ghack5.py
@@ -3,7 +3,7 @@ from bottle import route, run, debug, template, request
 
 @route('/violations')
 def violaitons():
-	conn = sqlite3.connect('ghack5')
+	conn = sqlite3.connect('ghack5-1.db')
 	c = conn.cursor()
 	#c.execute("SELECT id, task FROM todo WHERE status LIKE '0'")
 	#c.execute("SELECT id, violation_descriptions, citation_number FROM violations")
@@ -52,3 +52,8 @@ def citations():
 debug(True)
 #run(reloader=True, host='10.4.245.19', port=8080)
 run(reloader=True, port=8080)
+
+
+
+con.execute("CREATE TABLE citations (id INTEGER PRIMARY KEY, citation_number INTEGER, first_name text, last_name text, date_of_birth datetime, defendant_address text, defendant_city text, defendant_state text, court_date text, court_location text, court_address text)")
+con.execute("CREATE TABLE violations (id INTEGER PRIMARY KEY, citation_number INTEGER, violation_description text, warrant_status bool, warrant_number text, status text, fine_amount text, court_cost text)")
